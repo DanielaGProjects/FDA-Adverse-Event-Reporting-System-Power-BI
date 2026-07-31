3608c3edc02700585f8ea99210ac10d3490bae4f
DanielaGProjects <gomezdani9504@gmail.com>
2026-07-31 12:35:28 -0600

    readme was added and a file containing MedDRA controlled fields

--- Data ETL.py
+++ Data ETL.py
@@ -1,20 +1,56 @@
 import requests
 import json
 
-baseURL = "https://api.fda.gov/drug/event.json"
+#STEP 1. Fetching data from openFDA
 
-# Send the GET request
+def fetchDataAndCreateJSON():
+    # Send the GET request
+    response = requests.get("https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:'corticosteroid'+AND+receivedate:[20190101+TO+20191231]&limit=5")
+    myResponse = response.text
 
-response = requests.get("https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:'corticosteroid'&limit=5")
+    myJson = json.loads(myResponse)
 
-myResponse = response.text
-
-myJson = json.loads(myResponse)
-
-FDA_data= myJson["results"]
+    FDA_data= myJson["results"]
 
 
-with open("drugEvents.json", "w", encoding="utf-8") as file:
-    json.dump(FDA_data, file, ensure_ascii=False, indent=4)
+    with open("drugEvents.json", "w", encoding="utf-8") as file:
+        json.dump(FDA_data, file, ensure_ascii=False, indent=4)
 
-print("File saved!")
+    print("File saved!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Functions to call
+#fetchDataAndCreateJSON()
+
+
+
+
+
+
+
+
+
+
+
+
+"""1=recovered/resolved
+2=recovering/resolving
+3=not recovered/not
+resolved
+4=recovered/resolved
+with sequelae
+5=fatal
+6=unknown"""
